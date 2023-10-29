2b56ce037df440f940f3ea4774f94fe2fbd252ee
AhmadSaepulRahman <ahmadsaepul208@gmail.com>
2023-10-29 23:33:51 +0700

    laravel video 3

---
+++ coba1-laravel/routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Ahmad Saeful Rahman",
+        "email" => "ahmadsaepul208@gmail.com",
+        "image" => "ahmadsr.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
+});
+
+
