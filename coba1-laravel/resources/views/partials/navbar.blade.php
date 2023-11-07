d89d6e78d4f659df07a91d1aaac66ac01c19d9aa
AhmadSaepulRahman <ahmadsaepul208@gmail.com>
2023-11-07 22:38:57 +0700

    laravel video ke8

--- coba1-laravel/resources/views/partials/navbar.blade.php
+++ coba1-laravel/resources/views/partials/navbar.blade.php
@@ -15,6 +15,9 @@
           <li class="nav-item">
             <a class="nav-link {{ ($title === "posts")? 'active' : '' }}" href="/blog">Blog</a>
           </li>
+          <li class="nav-item">
+            <a class="nav-link {{ ($title === "posts")? 'active' : '' }}" href="/categories">Categories</a>
+          </li>
         </ul>
       </div>
     </div>
--- coba1-laravel/resources/views/post.blade.php
+++ coba1-laravel/resources/views/post.blade.php
@@ -3,8 +3,10 @@
 @section('container')
     <h1 class="mb-5">{{ $post->title }}</h1>
 
+    <p>By. Ahmad Saeful Rahman In <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></P>
+
     {!! $post->body !!}
 
-    <a href="/Posts">back to posts</a>
+<a href="/blog">BACK</a>
 
-    @endsection
+@endsection
