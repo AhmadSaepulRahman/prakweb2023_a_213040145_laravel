feeff9f49dcb83c20134c5e0556365d69af06b41
AhmadSaepulRahman <ahmadsaepul208@gmail.com>
2023-11-27 21:31:52 +0700

    laravel video ke18

--- coba1-laravel/routes/web.php
+++ coba1-laravel/routes/web.php
@@ -3,7 +3,7 @@
 use App\Models\Category;
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostControler;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\DashboardPostController;
@@ -37,9 +37,10 @@ Route::get('/about', function () {
 });
 
 
+Route::get('/posts', [PostController::class, 'index']);
 
-Route::get('/blog', [PostController::class, 'index']);
-Route::get('posts/{post:slug}', [PostController::class, 'show']);
+// Halaman Single Post
+Route::get('/posts/{post:slug}', [PostController::class, 'show']);
  
 Route::get('/categories', function() {
     return view('categories', [
@@ -62,41 +63,6 @@ Route::get('/dashboard', function() {
     return view('dashboard.index');
 })->middleware('auth');
 
+Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug'])->middleware('auth');
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Route::get('/categories/{category:slug}', function(Category $category){
-//     return view ('posts',[
-//         'title' => "Post By Category : $category->name",
-//         'active' => 'categories',
-//         'posts' => $category->posts->load('category','author'),
-//     ]);
-// });
-
-// Route::get('/authors/{author:username}', function(User $author){
-//     return view ('posts', [
-//         'title' => "Post By Author : $author->name",
-//          'active' => 'posts'
-//         'posts' => $author->posts->load('category','author'),
-//     ]);
-// });
